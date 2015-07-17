0c2632a7536423ef1d7b37c15a57b38b026671b6
Justin Edwards <justin@igrowbeards.com>
2015-07-16 19:26:52 -0500

    initial commit

---
+++ task_timer.rb
@@ -0,0 +1,21 @@
+class TaskTimer
+  # for each task in tasks
+    # get time for task
+    # get time for any dependant tasks -r
+    # add task.time to times array
+  # return largest number
+  
+  def intialize(tasks)
+    @tasks = tasks
+  end
+
+  def time_with_dependencies
+  end
+
+  def longest_time
+  end
+
+  def pretty_output
+  end
+
+end
